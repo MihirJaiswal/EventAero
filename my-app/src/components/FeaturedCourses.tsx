fa7b27ff7a68cab1b5f52d0e6e8012345067c81b
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2024-04-07 18:38:20 +0530

    some changes

--- my-app/src/components/FeaturedCourses.tsx
+++ my-app/src/components/FeaturedCourses.tsx
@@ -28,7 +28,7 @@ function FeaturedCourses() {
       <div className='mt-10'>
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center items-center mx-auto'>
             {featuredCourses.map((course:Course) => (
-                <div key={course.id} className="flex jusify-center items-center w-full pl-8">
+                <div key={course.id} className="flex jusify-center items-center w-full md:pl-8 p-4">
                     <BackgroundGradient  className="flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm">
                         <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
                         <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">{course.title}</p>
