3d5111022e31a439c5dafca59934dd02a54f2146
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2024-04-07 13:39:56 +0530

    some changes

---
+++ my-app/src/components/Navbar.tsx
@@ -0,0 +1,36 @@
+'use client'
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "@/components/ui/navbar-menu";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
+
+function Navbar({ className }: { className?: string }) {
+    const [active, setActive] = useState<string | null>(null);
+  return (
+    <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50 font-bold", className)}>
+      <Menu setActive={setActive}>
+        <Link href={"/"}>
+            <MenuItem setActive={setActive} active={active} item="Home">
+
+            </MenuItem>
+        </Link>
+        <MenuItem setActive={setActive} active={active} item="About">
+        <div className="flex flex-col space-y-2">
+            <HoveredLink href="/hobby">Hobby</HoveredLink>
+            <HoveredLink href="/individual">Individual</HoveredLink>
+            <HoveredLink href="/team">Team</HoveredLink>
+            <HoveredLink href="/enterprise">Enterprise</HoveredLink>
+        </div>
+        </MenuItem>
+        <Link href={"/contact"}>
+        <MenuItem setActive={setActive} active={active} item="Contact Us">
+        </MenuItem>
+        </Link>
+        
+
+      </Menu>
+    </div>
+  )
+}
+
+export default Navbar
